7b35a0704f3e58aacfcbfdfde621fff4491bea32
retrostreams <retrostreams@users.noreply.github.com>
2018-01-30 20:52:55 +0100

    remove dead store

--- src/main/java/org/openjdk/tests/tck/CompletableFutureTest.java
+++ src/main/java/org/openjdk/tests/tck/CompletableFutureTest.java
@@ -55,7 +55,7 @@ import junit.framework.TestSuite;
 
 @org.testng.annotations.Test
 public class CompletableFutureTest extends JSR166TestCase {
-// CVS rev. 1.192
+// CVS rev. 1.193
 
 //    public static void main(String[] args) {
 //        main(suite(), args);
@@ -304,7 +304,7 @@ public class CompletableFutureTest extends JSR166TestCase {
         }
 
         f = new CompletableFuture<>();
-        f.completeExceptionally(ex = new CFException());
+        f.completeExceptionally(new CFException());
         f.obtrudeValue(v1);
         checkCompletedNormally(f, v1);
         f.obtrudeException(ex = new CFException());
